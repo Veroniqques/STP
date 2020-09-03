46f7b5eededeb98c5892f16f3e575404645551ac
Veroniqques <70600595+Veroniqques@users.noreply.github.com>
2020-09-03 18:32:14 +0300

    task5 ready

---
+++ task5/src/com/veronica/Main.java
@@ -0,0 +1,10 @@
+package com.veronica;
+
+public class Main {
+    public static void main(String[] args) {
+	    double a=3;
+	    double b=4;
+	    double c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.println(c);
+    }
+}
