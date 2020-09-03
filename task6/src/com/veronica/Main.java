45ed1548fedb9ccf3a526a9d8bbc0b82c53a4006
Veroniqques <70600595+Veroniqques@users.noreply.github.com>
2020-09-03 18:43:59 +0300

    task6 ready

---
+++ task6/src/com/veronica/Main.java
@@ -0,0 +1,11 @@
+package com.veronica;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 1234;
+	String b = String.valueOf(a);
+	System.out.println(b.charAt(b.length()-1));
+
+    }
+}
