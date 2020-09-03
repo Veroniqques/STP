fd34933890340e6419abafc68036022a8b0958ce
Veroniqques <70600595+Veroniqques@users.noreply.github.com>
2020-09-03 19:21:47 +0300

    task10 ready

---
+++ task10/src/com/veronica/Main.java
@@ -0,0 +1,10 @@
+package com.veronica;
+
+public class Main {
+    public static float arithmean (int A, int B) {
+    return (A+B)/2;
+    }
+    public static void main(String[] args) {
+        System.out.println(arithmean(5,2));
+    }
+}
